16d3acd6c200dd56fd7c6ce5c6bed0fb5a585c86
Rafael M V <rafaelmendoncavaz@hotmail.com>
2024-04-02 15:45:24 -0300

    Versão Final: Atividades podem serem adicionadas ou removidas à vontade

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 // Array contendo Objeto com as Tasks
+// Atividades comentadas após a conclusão da Tarefa 4 - Removendo Tarefas
 const tasks = [
-  {title: "Comprar comida para o gato", type: "Urgente"},
-  {title: "Consertar Computador", type: "Importante"},
-  {title: "Beber água", type: "Normal"},
-  {title: "Enviar relatório trimestral", type: "Importante"},
-  {title: "Fazer exercícios físicos", type: "Normal"},
-  {title: "Agendar consulta médica", type: "Urgente"},
-  {title: "Ler pelo menos um capítulo de um livro", type: "Normal"},
-  {title: "Limpar a despensa", type: "Importante"},
-  {title: "Pagar a conta de energia", type: "Urgente"},
-  {title: "Assistir a um documentário interessante", type: "Normal"},
-];
+  // {title: "Comprar comida para o gato", type: "Urgente"},
+  // {title: "Consertar Computador", type: "Importante"},
+  // {title: "Beber água", type: "Normal"},
+  // {title: "Enviar relatório trimestral", type: "Importante"},
+  // {title: "Fazer exercícios físicos", type: "Normal"},
+  // {title: "Agendar consulta médica", type: "Urgente"},
+  // {title: "Ler pelo menos um capítulo de um livro", type: "Normal"},
+  // {title: "Limpar a despensa", type: "Importante"},
+  // {title: "Pagar a conta de energia", type: "Urgente"},
+  // {title: "Assistir a um documentário interessante", type: "Normal"},
+]
 
 // Função para Renderizar os elementos contidos no array
 function renderElements(arr) {
 
   // Capturando a tag de Lista
   const ulist = document.querySelector('.tasks__list')
+  ulist.innerHTML = ''
   
   // Iterando sobre o array com objeto tasks
   for(let i = 0; i < arr.length; i++) {
@@ -28,6 +30,7 @@ function renderElements(arr) {
 }
 
 function createTaskItem(title, type) {
+
    // Criando elementos com DOM
    const li = document.createElement('li')
    const div = document.createElement('div')
@@ -65,8 +68,47 @@ function createTaskItem(title, type) {
    div.appendChild(p)
    li.appendChild(button)
 
+  // Evento de remoção de Task
+  button.addEventListener('click', function(event) {
+    event.preventDefault()
+    
+    // Capturar o nome da tarefa associada ao botão
+    const taskTitle = this.parentNode.querySelector('p').innerText
+
+    // Buscar se o array contem a tarefa mencionada
+    const locateTask = tasks.findIndex(task => task.title === taskTitle)
+
+    // Condicional para deletar a tarefa
+    if (locateTask !== -1) {
+      tasks.splice(locateTask, 1)
+      renderElements(tasks)
+    } else {
+      console.error('Task not found')
+    }
+
+    event.stopPropagation()
+  })
+
    return li
 }
 
 // Renderizando as tasks contidas no Objeto do Array
-renderElements(tasks)
+// renderElements(tasks)
+
+// Adicionando Evento ao botão Adicionar Tarefa na Lista
+const button = document.querySelector('.form__button--add-task')
+
+button.addEventListener('click', function addTask(event) {
+  event.preventDefault()
+
+  const input = document.getElementById('input_title')
+  const select = document.querySelector('.form__input--priority')
+  const task = input.value
+  const priority = select.value
+
+  tasks.push({title: task, type: priority})
+
+  renderElements(tasks)
+
+  event.stopPropagation()
+})
